66238387b46d449960037c54046fdcf2a4a501e7
eka-24 <62040964+eka-24@users.noreply.github.com>
2020-11-30 19:54:53 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+// import SplashScreen from './src/pages/splashscreen';
+import Register from './src/pages/Register';
+import Login from './src/pages/login';
+import Home from './src/pages/home';
+import profil from './src/pages/home';
+
+
+
+
+ const AuthStack = createStackNavigator(
+ {
+  login: {screen : Login},
+  Register: {screen : Register},
+  profil: {screen:profil},
+  Home: {screen : Home}
+ 
+ 
+  
+ });
+
+ const MainStack = createStackNavigator({
+  Home: {screen: Home}
+ });
+
+ const Navigators = createSwitchNavigator({
+  Auth: { screen: AuthStack},
+  Main: { screen: MainStack}
+ });
+
+ export default createAppContainer(Navigators);
